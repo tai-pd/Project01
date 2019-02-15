d97b70eccbaebd3b69d056caa2cc8b4532abd1c7
Pham Duc Tai 2 <tai.phamduc2@gameloft.com>
2019-02-15 15:17:12 +0700

    laravel learning b8

---
+++ app/Http/Controllers/myController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class myController extends Controller
+{
+    public function m_controller1()
+    {
+    	echo 'xin chao';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,46 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('HelloLavarel',
+function(){
+	return 'Hello world';
+});
+
+Route::get('HelloLavarel/today/{today}', function($today){
+	return 'hom nay la ngay : ' .$today;
+});
+
+Route::get('Route1/{route1}', ['as'=>'my_Route1', function($text){
+	return 'this is my route :' .$text;
+}]);
+
+Route::get('Route2', function(){
+	echo 'this is round 2';
+})->name('my_Round2');
+
+Route::get('GetRoute1', function($text){
+	return redirect()->route('my_Route1', $text);
+});
+
+Route::get('GetRound2', function(){
+	return redirect()->route('my_Round2');
+});
+
+Route::group(['prefix'=>'myGroup'], function(){
+	Route::get('User1', function(){
+		echo 'user1';
+	});
+
+	Route::get('User2', function(){
+		echo 'user2';
+	});
+
+	Route::get('User3',function(){
+		echo 'user3';
+	});
+});
+
+Route::get('goiController1', 'myController@m_controller1');
+
+
